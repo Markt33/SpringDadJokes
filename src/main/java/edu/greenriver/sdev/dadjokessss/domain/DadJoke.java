6c265a3a3aea6b694048346ce6a407b4b2fc45f9
markt33 <marktsvyan24@gmail.com>
2024-01-18 14:20:58 -0800

    Created a DadJoke class with to fields and annotated the class to use Lombok.

---
+++ src/main/java/edu/greenriver/sdev/dadjokessss/domain/DadJoke.java
@@ -0,0 +1,21 @@
+package edu.greenriver.sdev.dadjokessss.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class DadJoke {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String jokeText;
+}
