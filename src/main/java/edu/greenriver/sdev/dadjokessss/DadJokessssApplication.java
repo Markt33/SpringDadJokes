6aa80d6b2039efda4b6707fde646ffeda92f428a
markt33 <marktsvyan24@gmail.com>
2024-01-18 14:05:57 -0800

    Created a Dad Jokes App.

---
+++ src/main/java/edu/greenriver/sdev/dadjokessss/DadJokessssApplication.java
@@ -0,0 +1,13 @@
+package edu.greenriver.sdev.dadjokessss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadJokessssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DadJokessssApplication.class, args);
+    }
+
+}
