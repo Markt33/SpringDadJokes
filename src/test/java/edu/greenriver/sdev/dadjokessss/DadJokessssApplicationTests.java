78f29f7d41cefb09dd04d07b5c9c873c5658bfcb
markt33 <marktsvyan24@gmail.com>
2024-01-23 13:46:43 -0800

    Created several Dad joke objects & saved them to the database.

---
+++ src/test/java/edu/greenriver/sdev/dadjokessss/DadJokessssApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.greenriver.sdev.dadjokessss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadJokessssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
